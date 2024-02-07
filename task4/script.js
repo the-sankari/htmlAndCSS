6b349a413fcda6b9779400d4be67dba6fe59be13
Kajol Sutra dhar <kajol.777@gmail.com>
2024-02-07 11:49:42 +0200

    Update files and submit task 5

--- task4/script.js
+++ task4/script.js
@@ -6,11 +6,23 @@ const time = document.querySelector("#time");
 const showTime = document.querySelector("#timeValue");
 showTime.textContent = time.value;
 
-distance.addEventListener("input",(event)=>{
+const speed = document.querySelector(".speed");
+const calculateBtn = document.querySelector(".calculate");
+
+
+const dValue= distance.addEventListener("input",(event)=>{
     showDistance.textContent = event.target.value;
 })
 
-time.addEventListener("input",(event)=>{
-    showDistance.textContent = event.target.value;
+const tValue = time.addEventListener("input",(event)=>{
+    showTime.textContent = event.target.value;
 })
+function calSpeed() {
+     let speed = distance.value / time.value;
+     return speed.toFixed(2);
+}
 
+
+calculateBtn.addEventListener("click",()=>{
+    speed.textContent = `Speed: ${calSpeed()} km/h`;
+})
